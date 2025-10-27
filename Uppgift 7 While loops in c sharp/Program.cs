7a03f34922d530bee1b924e673152fe88a4216c6
Tomas708L <tomas.langstedt@gmail.com>
2025-10-27 11:49:00 +0200

    Add files via upload

---
+++ Uppgift 7 While loops in c sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_7_While_loops_in_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+
+            while (counter <= 5)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.WriteLine("The loop has been finished");
+
+        }
+    }
+}
