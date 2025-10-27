61f808fa9d9479441c2706c3ef1f9a9b7df0788c
Tomas708L <tomas.langstedt@gmail.com>
2025-10-27 11:23:28 +0200

    Add files via upload

---
+++ uppgift 4 if statement in c-sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_4_if_statement_in_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+
+            if(age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+            else if(age < 18)
+            {
+                Console.WriteLine("You are not eligible to vote");
+
+            }
+        }
+    }
+}
