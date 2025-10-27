71c4826992994c62f35c3d5718128b780e8c36b2
Tomas708L <tomas.langstedt@gmail.com>
2025-10-27 11:32:54 +0200

    Add files via upload

---
+++ Uppgift 5 Switch Statement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_5_Switch_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your grade: ");
+            string grade = Console.ReadLine();
+            int realGrade = Convert.ToInt32(grade);
+
+            switch(realGrade)
+            {
+                case 0:
+                    Console.WriteLine("Fail");
+                    break;
+                case 1:
+                    Console.WriteLine("Needs Improvement");
+                    break;
+                case 2:
+                    Console.WriteLine("Ok");
+                    break;
+                case 3:
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 4:
+                    Console.WriteLine("Good");
+                    break;
+                case 5:
+                    Console.WriteLine("Excellent");
+                    break;
+
+            }
+        }
+    }
+}
