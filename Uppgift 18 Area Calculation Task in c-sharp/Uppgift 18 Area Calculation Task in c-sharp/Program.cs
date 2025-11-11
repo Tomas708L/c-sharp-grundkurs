046a801f216f944856864d644af1979d8f0d43a1
Tomas708L <tomas.langstedt@gmail.com>
2025-11-11 14:55:52 +0200

    Add files via upload

---
+++ Uppgift 18 Area Calculation Task in c-sharp/Uppgift 18 Area Calculation Task in c-sharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_18_Area_Calculation_Task_in_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            string HeightInString = Console.ReadLine();
+            int Height = Convert.ToInt32(HeightInString);
+
+            Console.Write("Enter widgth: ");
+            string WidgthInString = Console.ReadLine();
+            int Widgth = Convert.ToInt32(WidgthInString);
+
+            int result = Calc(Widgth, Height);
+
+            Console.WriteLine($"Here is the area of a triangle: {result}");
+
+        }
+
+        static int Calc(int Widgth, int Height)
+        {
+            return (Widgth * Height) / 2;
+        }
+
+    }
+}
