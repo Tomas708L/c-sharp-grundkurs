f88d8c71b9506572e7910516bff3e46f9966cdc9
Tomas708L <tomas.langstedt@gmail.com>
2025-11-11 10:25:11 +0200

    Add files via upload

---
+++ Uppgift 14 Array multiplication in c-sharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_14_Array_multiplication_in_c_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7;
+            int length = 6;
+            int[] numbers = new int[length];
+
+            for(int i = 1; i < length; i++)
+            {
+                numbers[i] = i * num;
+            }
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
