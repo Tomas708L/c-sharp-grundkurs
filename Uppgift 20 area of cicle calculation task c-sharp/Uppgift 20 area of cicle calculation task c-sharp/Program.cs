7d4537db88088f76638f370b440f7b55be4a6163
Tomas708L <tomas.langstedt@gmail.com>
2025-11-12 09:43:59 +0200

    Add files via upload

---
+++ Uppgift 20 area of cicle calculation task c-sharp/Uppgift 20 area of cicle calculation task c-sharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_20_area_of_cicle_calculation_task_c_sharp
+{
+    class Circle
+    {
+        public double Radius
+        {
+            get; set;
+        }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius *Radius;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle RadiusCircle = new Circle(1.5);
+
+            double Area = RadiusCircle.CalculateArea();
+
+            Console.WriteLine($"The Radius: {RadiusCircle}");
+            Console.WriteLine($"The Area: {Area}");
+        }
+    }
+}
