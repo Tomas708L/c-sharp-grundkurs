cb67f0479ab4beb1f771e53262cac73f23ff975c
Tomas708L <tomas.langstedt@gmail.com>
2025-11-12 09:15:13 +0200

    Add files via upload

---
+++ Uppgift 19 integer array task in c#/Uppgift 19 integer array task in c#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_19_integer_array_task_in_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, };
+
+            int sum = SumOfNumbers(numbers);
+            
+            Console.WriteLine("Here is the sum off all numbers in the array: " + sum);
+
+            Console.WriteLine($"This is the amount of numbers in the array: {numbers.Length}");
+
+            if(sum > -1)
+            {
+                Console.WriteLine($"The total is: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot add up an empty array");
+            }
+
+
+        }
+        static int SumOfNumbers(int[] numbers)
+        {
+            return numbers.Sum(x => x);
+        }
+    }
+}
---
+++ Uppgift 19 integer array task in c#/Uppgift 19 integer array task in c#/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uppgift 19 integer array task in c#")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uppgift 19 integer array task in c#")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a7bd25d-41f2-471f-82db-509f465f6e40")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
