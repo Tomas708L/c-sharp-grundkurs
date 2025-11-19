b3828b4d38d3d978f74514f76a27575c51d90cf8
Tomas708L <tomas.langstedt@gmail.com>
2025-11-19 10:43:05 +0200

    Add files via upload

---
+++ Uppgift 25 Lists and collections in c-sharp/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_25_Lists_and_collections_in_c_sharp
+{
+    internal class Program
+    {
+        class Inventory
+        {
+            private List<string> items = new List<string>();
+
+            private Dictionary<string, (double price, int stock)> ItemDetails =
+                new Dictionary<string, (double price, int stock)>();
+
+            //Adds item
+            public void AddItem(string ProductName, double price, int stock)
+            {
+                if(!items.Contains(ProductName))
+                {
+                    items.Add(ProductName);
+                    ItemDetails[ProductName] = (price, stock);
+                }
+                else
+                {
+                    Console.WriteLine($"{ProductName} allredy exist");
+                }
+            }
+
+                //Removes item
+                public void removeItem(string ProductName)
+            {
+                if(!items.Contains(ProductName))
+                {
+                    items.Remove(ProductName);
+                }
+                else
+                {
+                    Console.WriteLine($"{ProductName} does not excist");
+                }
+            }
+
+            //Updates item
+            public void UpdateStock(string  ProductName, int updatedStock)
+            {
+                if(items.Contains(ProductName))
+                {
+                    double price = ItemDetails[ProductName].price;
+                    ItemDetails[ProductName] = (price, updatedStock);
+                }
+                else
+                {
+                    Console.WriteLine($"{ProductName} cannot be found");
+                }
+            }
+
+            //Print all items
+            public void PrintAllItems()
+            {
+                if(items.Count == 0)
+                {
+                    Console.WriteLine("The inventory is empty");
+                    return;
+                }
+                foreach(string item in items)
+                {
+                    var (price, stock) = ItemDetails[item];
+                    Console.WriteLine($"{item}: Price: ${price}, Stock: {stock}");
+                }
+            }
+
+
+            //Prints the most expensive item
+            public void mostExpensive()
+            {
+                double TheHighestPrice = 0;
+                string TheMostExpensive = null;
+
+                if (items.Count == 0)
+                {
+                    return;
+                }
+                foreach(string item in items)
+                {
+                    double price = ItemDetails[item].price;
+                    if (price > TheHighestPrice)
+                    {
+                        TheHighestPrice = price;
+                        TheMostExpensive = item;
+                    }
+                }
+                Console.WriteLine("The Most expensive item: " + TheMostExpensive + " The price is: " + TheHighestPrice);
+            }
+
+
+            //Prints the total inventory
+            public void TotalInventory()
+            {
+                double total = 0;
+
+                foreach (string item in items)
+                {
+                    var (price, stock) = ItemDetails[item];
+                    total += price * stock;
+                }
+                Console.WriteLine($"This is the total inventory: ${total:F2}");
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            //Here i call for my methods in the class
+            Inventory inventory = new Inventory();
+
+            inventory.AddItem("Laptop", 1200.50, 7);
+            inventory.AddItem("SmartPhone", 799.99, 12);
+            inventory.AddItem("Tablet", 399.99, 15);
+
+            inventory.PrintAllItems();
+
+
+            inventory.UpdateStock("Laptop", 5);
+
+            inventory.removeItem("Tablet");
+
+            inventory.mostExpensive();
+
+            inventory.PrintAllItems();
+
+        }
+    }
+}
---
+++ Uppgift 25 Lists and collections in c-sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uppgift 25 Lists and collections in c-sharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uppgift 25 Lists and collections in c-sharp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("806ef447-ea44-4f8e-ad3a-aabb7b6b92e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
