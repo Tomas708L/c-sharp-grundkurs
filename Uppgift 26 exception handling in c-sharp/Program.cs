5fcb314d32dd46c3ef27428ec1840660785a0088
Tomas708L <tomas.langstedt@gmail.com>
2025-11-20 11:06:07 +0200

    Refactor Calculator methods for better error handling
    Updated division method to handle zero division without exception. Improved input parsing and output display.

--- Uppgift 26 exception handling in c-sharp/Program.cs
+++ Uppgift 26 exception handling in c-sharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,63 +10,61 @@ namespace Uppgift_26_exception_handling_in_c_sharp
     internal class Program
     {
         class Calculator
-
         {
-            //Here is the method that will divide the 2 numbers
-            public static double Divide(int x, int y)
+
+            //Here is the method that will divide 2 integers numbers
+            public static double Divide(double a, double b)
             {
-                try
+                if(b == 0)
                 {
-                    
-                    return (double)x / y;
-                }
-                catch(DivideByZeroException)
-                {
-                    Console.WriteLine("Can't divide with zero");
+                    Console.WriteLine("Cannot devide with 0");
                     return 0.0;
                 }
-
-                
-                
+                return a / b;
             }
-            //Here is the method that will convert string numbers to int, 
-            public static (int num1, int num2) ParseInput(string strnum1, string strnum2)
+
+            
+            //The method that converts string to int
+            public static (int, int) ParseInput(string Input1, string Input2)
             {
                 try
                 {
-                    
-                    int InputNum1 = Convert.ToInt32(strnum1);
-                    int InputNum2 = Convert.ToInt32(strnum2);
-
-                    return (InputNum1, InputNum2);
-                    
-
+                    int num1 = Convert.ToInt32(Input1);
+                    int num2 = Convert.ToInt32(Input2);
+                    return (num1,  num2);
                 }
                 catch(FormatException)
                 {
-                    Console.WriteLine("The input was not a number!");
+                    Console.WriteLine("Only use numbers");
                     return (-1, -1);
-
                 }
-                
-
             }
+
+
         }
+        //The main method
         static void Main(string[] args)
         {
-            
+            //Ask the user for numbers
             Console.Write("Enter a number: ");
             string num1 = Console.ReadLine();
 
             Console.Write("Enter a second number: ");
             string num2 = Console.ReadLine();
 
-            //Here i used the methods
-            var Result = Calculator.ParseInput(num1, num2);
+            
+            //Here i call for the methods
+            var Conversion = Calculator.ParseInput(num1, num2);
 
-            double divide = Calculator.Divide(Result.num1, Result.num2);
+            var result = Calculator.Divide(Conversion.Item1, Conversion.Item2);
+
+            Console.WriteLine("The result of the division is: " + result);
 
             
+        }
+    }
+}
+
 
             
             
@@ -75,3 +73,4 @@ namespace Uppgift_26_exception_handling_in_c_sharp
         }
     }
 }
+
